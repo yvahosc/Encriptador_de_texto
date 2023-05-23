f2132efc787a7d7392e055c1ea570d33546e65e2
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-05-23 14:14:15 -0500

    Creación de la lógica para la encriptación y desencriptación en el archivo script.js y su enlace al archivo HTML.

---
+++ script.js
@@ -0,0 +1,52 @@
+const textArea = document.querySelector('.text-area');
+const mensaje = document.querySelector('.mensaje');
+
+const llavesEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+function encriptar(stringAEncriptar){
+    stringAEncriptar = stringAEncriptar.toLowerCase();
+    let stringEncriptada = stringAEncriptar;
+
+    for(let i = 0; i < llavesEncriptacion.length; i++){
+        if (stringAEncriptar.includes(llavesEncriptacion[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnEncriptar(){
+    const stringEncriptada = encriptar(textArea.value);
+    mensaje.value = stringEncriptada;
+    textArea.value ="";
+}
+
+function desencriptar(stringADesencriptar){
+    stringADesencriptar = stringADesencriptar.toLowerCase();
+    let stringDesencriptada = stringADesencriptar;
+
+    for(let i = 0; i < llavesEncriptacion.length; i++){
+        if (stringADesencriptar.includes(llavesEncriptacion[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function btnDesencriptar(){
+    const stringDesencriptada = desencriptar(textArea.value);
+    mensaje.value = stringDesencriptada;
+    textArea.value ="";
+}
+
+function copy(){
+    let textoACopiar = mensaje;
+    textoACopiar.select();
+    document.execCommand("copy");
+}
+
+
+function btnCopiar(){
+    copy();
+    alert("Elemento copiado.");
+}
